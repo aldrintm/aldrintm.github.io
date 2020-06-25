673f78d73728a124e9a610a008da0291d45ce8ed
aldrintm <aldrin@plamarusa.com>
2020-06-25 01:52:50 -0700

    Update myscripts.js

--- js/myscripts.js
+++ js/myscripts.js
@@ -2,7 +2,7 @@ const myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
 
 function init() {
-Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/0AmYzu_s7QHsmdDNZUzRlYldnWTZCLXdrMXlYQzVxSFE/pubhtml',
+Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/1p5MR2t2dbBeUb2Kvpx98ncKCIHfkb5Y57nF2oYlvRCg/pubhtml',
 callback: function(data, tabletop) {
 console.log(data)
 },
